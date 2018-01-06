f5c58b5ba3d079bd6ca14a68926911c984e6dab4
Hong Zhuang <zhuanghong2013@gmail.com>
2018-01-06 23:57:39 +0000

    modified function

--- seeds.js
+++ seeds.js
@@ -10,6 +10,7 @@ var data = [
         name: "Five Spice Rolls/Chinese Meat Rolls 五香", 
         image: "http://www.whatsonxiamen.com/news_images/72082.jpg",
         description: "This is a snack made of minced meat (usually pork) and vegetables wrapped in a dry bean curd sheet. Although sold in stalls or small eateries, homemade Chinese Meat Rolls are far superior to those sold commercially."
+        
     },
     {
         name: "Lor Mee/Chinese Braised Noodles 卤面", 
@@ -23,41 +24,51 @@ var data = [
     }
 ]
 
+// function seedDB(){
+//   //Remove all streetfoods
+//   Streetfood.remove({}, function(err){
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log("removed streetfoods!");
+//         //add a few streetfoods
+//         data.forEach(function(seed){
+//             Streetfood.create(seed, function(err, streetfood){
+//                 if(err){
+//                     console.log(err)
+//                 } else {
+//                     console.log("added a streetfood");
+//                     //create a comment
+//                     Comment.create(
+//                         {
+//                             text: "Zhangzhou's snackes are the best in the world!",
+//                             author: "Hong"
+//                         }, function(err, comment){
+//                             if(err){
+//                                 console.log(err);
+//                             } else {
+//                                 //Associate with streetfood, push them into the array.
+//                                 //And then save the streetfood.
+//                                 streetfood.comments.push(comment);
+//                                 streetfood.save();
+//                                 console.log("Created new comment");
+//                             }
+//                         });
+//                 }
+//             });
+//         });
+//     }); 
+//     //add a few comments
+// }
+
 function seedDB(){
-   //Remove all streetfoods
-   Streetfood.remove({}, function(err){
-        if(err){
-            console.log(err);
-        }
-        console.log("removed streetfoods!");
-        //add a few streetfoods
-        data.forEach(function(seed){
-            Streetfood.create(seed, function(err, streetfood){
-                if(err){
-                    console.log(err)
-                } else {
-                    console.log("added a streetfood");
-                    //create a comment
-                    Comment.create(
-                        {
-                            text: "Zhangzhou's snackes are the best in the world!",
-                            author: "Hong"
-                        }, function(err, comment){
-                            if(err){
-                                console.log(err);
-                            } else {
-                                //Associate with streetfood, push them into the array.
-                                //And then save the streetfood.
-                                streetfood.comments.push(comment);
-                                streetfood.save();
-                                console.log("Created new comment");
-                            }
-                        });
-                }
-            });
-        });
-    }); 
-    //add a few comments
+    //Remove all streetfoods
+    Streetfood.remove({}, function(err){
+         if(err){
+             console.log(err);
+         }
+         console.log("removed streetfoods!");
+    });
 }
 
 module.exports = seedDB;
