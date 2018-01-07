c362e6996985f1995e75a468628f1e2f55eca6c8
Hong Zhuang <zhuanghong2013@gmail.com>
2018-01-07 05:02:08 +0000

    change background

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -2,7 +2,7 @@ var counter = 0;
 function changeBG(){
     var imgs = [
         "url(http://www.slyar.com/blog/wp-content/uploads/2012/08/DSC_0804.jpg)",
-        "url(https://i.pinimg.com/736x/12/3e/5e/123e5ea22e11bd2368ebfab1d31ef8b4--japanese-street-food-japanese-food.jpg)",
+        "url(https://media.cntraveler.com/photos/59baad8b56085e35b15e7622/master/pass/Bangkok_GettyImages-sb10065342aq-001.jpg)",
         "url(http://www.slyar.com/blog/wp-content/uploads/2012/08/DSC_0805.jpg)"
       ]
     
